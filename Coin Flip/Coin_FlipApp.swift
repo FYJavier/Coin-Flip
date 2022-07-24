afbeb7d972b4c89d1f07f4de58af901c68644370
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-24 16:29:35 -0500

    Initial Commit

---
+++ Coin Flip/Coin_FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_FlipApp.swift
+//  Coin Flip
+//
+//  Created by Yuna on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Coin_FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin Flip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coin Flip
+//
+//  Created by Yuna on 7/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
