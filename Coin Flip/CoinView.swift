0641736912be21dfb6a16aba1be188051e39a136
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-24 20:38:22 -0500

    Created prediction picker

---
+++ Coin Flip/CoinView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinView.swift
+//  Coin Flip
+//
+//  Created by Yuna on 7/24/22.
+//
+
+import SwiftUI
+
+struct CoinView: View {
+    var phrase: Int
+    var body: some View {
+    }
+}
+
+struct CoinView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinView(phrase: 1)
+    }
+}
--- Coin Flip/ContentView.swift
+++ Coin Flip/ContentView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var prediction = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Enter your prediction")
+                    .font(.system(size: 25))
+                    .padding()
+                Picker("", selection: $prediction) {
+                    Text("Heads").tag(1)
+                    Text("Tails").tag(2)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .frame(width: 350, height: 20, alignment: .center)
+                .padding()
+                .padding(.bottom, 30)
+                .font(.system(size: 18))
+            }
+            .padding(.bottom, 150)
+            .navigationTitle("Coin Flip")
+            Spacer()
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
