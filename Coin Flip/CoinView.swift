6583d124ac9dce8bb12714fd4ad17c387abd01e1
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-29 01:15:19 -0500

    Created rotation (finished project)

--- Coin Flip/CoinView.swift
+++ Coin Flip/CoinView.swift
@@ -9,48 +9,69 @@ import SwiftUI
 
 struct CoinView: View {
     @State private var isRotated = false
-    @State private var isTails = false
     @State private var coinImage = "Heads"
     @State private var gameOver = false
     @State private var winMessage = ""
-    @State var isFaceUp = false
+    @State private var isPlaying = false
     @Environment(\.presentationMode) var presentationMode
     var animation: Animation {
-        Animation.easeInOut
-    .repeatCount(15)
-    .speed(1.0)
+        Animation.linear
+            .repeatForever(autoreverses: false)
+            .speed(2.0)
     }
     let prediction: Int
     var body: some View {
+        
         VStack {
-            Text("Flip the coin")
-            Button(action: {
-                buttonPressed()
-                    
-            }) {
-                ZStack {
-                    Image(coinImage)
-                        .resizable()
-                        .frame(width: 300, height: 300, alignment: .center)
-                            
-                }
+            Button {
+                self.isRotated.toggle()
+            } label: {
+                Text("Flip the coin")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                    .frame(width: 500, height: 200, alignment: .center)
             }
-                .alert(isPresented: $gameOver) {
-                    Alert(title: Text(winMessage),dismissButton: .destructive(Text("Play again"), action: {
-                        withAnimation {
-                            coinImage = "Heads"
-                            gameOver = false
-                            self.presentationMode.wrappedValue.dismiss()
-                        }
-                    }))
-                    
-                }
-           
+            ZStack {
+                Image(coinImage)
+                    .resizable()
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .rotation3DEffect(Angle.degrees(isRotated ? 360 : 0), axis: (x: 1, y: 0, z: 0))
+                    .animation(animation)
+                    .opacity(isPlaying ? 0 : 1)
+                
+                Image(coinImage)
+                    .resizable()
+                    .frame(width: 350, height: 350, alignment: .center)
+                    .opacity(isPlaying ? 1 : 0)
+            }
+            
+            Button {
+                buttonPressed()
+                self.isPlaying.toggle()
+            } label: {
+                Text("See the result!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 500, height: 200, alignment: .center)
+                    .padding(.bottom, 100)
+            }
+            
+            .alert(isPresented: $gameOver) {
+                Alert(title: Text(winMessage),dismissButton: .destructive(Text("Play again"), action: {
+                    withAnimation {
+                        coinImage = "Heads"
+                        gameOver = false
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                }))
+            }
+            
         }
     }
     
     func buttonPressed() {
-        
         if Int.random(in: 0...1) == 0 {
             coinImage = "Heads"
             if prediction == 0 {
@@ -72,7 +93,6 @@ struct CoinView: View {
             }
         }
     }
-    
 }
 
 struct CoinView_Previews: PreviewProvider {
